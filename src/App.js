1c5642b2507bce9b3c5df866392b91fba9cb2398
GustavoDeveloper1 <devinvestidor02@gmail.com>
2021-03-10 23:03:46 -0300

    adicionando estilização

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import Routes from './routes';
-import Header from './components/Header';
+
 
 import history from './services/history';
 import store from './store';
+import Header from './components/Header/index';
 
 export default function App() {
  return (
    <Provider store={store}>
     <Router history={history}>
       <Header/>
+
       <Routes/>
     </Router>
    </Provider>
